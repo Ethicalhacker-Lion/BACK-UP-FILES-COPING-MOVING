9d666e8f327d21816febb04c0d6f55ea17a3e053
Ethicalhacker-Lion <74255405+Ethicalhacker-Lion@users.noreply.github.com>
2022-03-07 18:27:19 +0530

    Add files via upload

---
+++ WH-project2.py
@@ -0,0 +1,32 @@
+# BACKUP FILE FOR COPING
+
+# import os
+# import shutil
+#
+# source = input("Enter the source folder name:- ")
+# destination = input("Enter the destination name:- ")
+#
+# source = source+'/'
+# destination = destination+'/'
+#
+# list_of_files = os.listdir(source)
+#
+# for check in list_of_files:
+#     shutil.copy((source+check), destination)
+
+# BACKUP FILE FOR  MOVING
+
+# import os
+# import shutil
+#
+# source = input("Enter the source folder name:- ")
+#
+# destination = input("Enter the destination name:- ")
+# source = source+'/'
+# destination = destination+'/'
+#
+# list_of_files = os.listdir(source)
+#
+# for check in list_of_files:
+#     shutil.move((source+check), destination)
+
